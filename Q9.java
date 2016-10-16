3274224cb170b00f849a72d170c6dd8dec0cba0a
Vibhor <bijoy.vibhor19@gmail.com>
2016-10-16 20:36:57 +0530

    Create Q9.java

---
+++ Q9.java
@@ -0,0 +1,37 @@
+package com.bijoy.vibhor.flagquiz;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Q9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q9);
+        TextView totalScoreTextView = ((TextView)findViewById(R.id.total_score));
+        totalScoreTextView.setText(totalScoreTextView.getText() + " " +((FlagQuiz)getApplicationContext()).totalScore);
+    }
+
+    public void checkanswer(View v) {
+        EditText et7 = (EditText) findViewById(R.id.et9);
+        if (et7.getText().toString().trim().toLowerCase().equals("columbia")) {
+            Toast toast = Toast.makeText(Q9.this, "Correct answer +5", Toast.LENGTH_SHORT);
+            toast.getView().setBackgroundColor(Color.GREEN);
+            toast.show();
+            ((FlagQuiz)getApplicationContext()).totalScore += 5;
+        } else {
+            Toast toast = Toast.makeText(Q9.this, "Incorrect answer O points", Toast.LENGTH_SHORT);
+            toast.getView().setBackgroundColor(Color.RED);
+            toast.show();
+        }
+        startActivity(new Intent(Q9.this, Q10.class));
+        this.finish();
+    }
+}
